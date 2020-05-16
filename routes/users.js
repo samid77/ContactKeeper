f552d137e2138229100697cea128c5c8d5bafc77
Samid77 <dimaseptyanto@windowslive.com>
2020-05-16 16:08:27 +0700

    Create registration method using bcrypt and jsonwebtoken

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
 
 // @route     POST api/users
 // @desc      Register a user
 // @access    Public
-router.post('/', (req, res) => {
-    res.send('Register a user');
-});
+router.post(
+    '/', 
+    [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty(),
+        check('email', 'Insert a valid email')
+            .isEmail(),
+        check('password', 'Please enter a password with 6 or more characters')
+            .isLength({min: 6}),
+    ], 
+    async (req, res) => {
+        
+        //Validate input body
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()});
+        }
+        const { name, email, password } = req.body;
+        try {
+         
+         //Check if the user alrerady exists
+         let user = await User.findOne({email: email});
+         if(user) {
+             return res.status(400).json({ msg: 'User already exists'});
+         }
+
+         //Proceed to bind the input data into object
+         user = new User({
+             name: name,
+             email: email,
+             password: password
+         })
+
+         //Hash the inputted password
+         const salt = await bcrypt.genSalt(10);
+         user.password = await bcrypt.hash(password, salt);
+
+         //Save to database
+         await user.save();
+         const payload = {
+             user: {
+                 id: user.id
+             }
+         }
+         jwt.sign(
+             payload, 
+             config.get('jwtSecret'), 
+             {expiresIn: 360000},
+             (err, token) => {
+                 if(err) throw err;
+                 res.json({ token });
+             }
+         )
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Error in saving user data');
+        }
+    }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const connnectDB = require('./config/db');
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -8,9 +11,15 @@ app.get('/', (req, res) => {
     });
 });
 
+//Connect to Database
+connnectDB();
+
+//Init Middleware
+app.use(express.json({extended: false}))
+
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started at port ${PORT} ...`));
