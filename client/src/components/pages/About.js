02b9aa270e7392d6ad3cbd24ae928d256d520620
Samid77 <dimaseptyanto@windowslive.com>
2020-05-18 08:29:01 +0700

    Adding context functionality

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p className="my-1">This app build using React, Express & MongoDB</p>
+            <p className="bg-success p">
+                <strong>Version:</strong>1.0.0
+            </p>
+        </div>
+    )
+}
+
+export default About
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,76 @@
+import React, { useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACT,
+    CLEAR_FILTER
+} from '../types';
+
+const ContactState = (props) => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'Arthur Morgan',
+                email: 'arthur@mail.com',
+                phone: '111-111-111',
+                type: 'professional'
+            },
+            {
+                id: 2,
+                name: 'John Marston',
+                email: 'johnmarston@mail.com',
+                phone: '222-222-222',
+                type: 'personal'
+            },
+            {
+                id: 3,
+                name: 'Winter Richard',
+                email: 'winrichard@mail.com',
+                phone: '333-333-333',
+                type: 'personal'
+            },
+
+            {
+                id: 4,
+                name: 'Liebgott Andrew',
+                email: 'lib.andrew@mail.com',
+                phone: '444-44-444',
+                type: 'personal'
+            },
+        ],
+    };
+
+    const [state, dispatch] = useReducer(contactReducer, initialState);
+
+    // Add Contact
+
+    // Delete Contact
+
+    // Update Contact
+
+    // Filter Contacts
+
+    // Clear Filter
+
+    // Set Current Contact
+
+    // Clear Current Contact
+
+    return (
+        <ContactContext.Provider
+            value={{
+                contacts: state.contacts
+            }}>
+            {props.children}
+        </ContactContext.Provider>
+    );
+}
+
+export default ContactState;
